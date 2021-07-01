13d42015219b218ad4fb5c663cda325763b59566
Nestor Matias <solomatias321@gmail.com>
2021-07-01 18:04:52 +0200

    disable authguard to authentications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes =[
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
   {
-    path: 'home', component: HomeComponent, canActivate: [AuthGuard]
+    path: 'home', component: HomeComponent, //canActivate: [AuthGuard]
   },
   { path: 'forgot-password', component: ForgotPasswordComponent},
   { path: 'verify-email-address', component: VerifyEmailComponent},
